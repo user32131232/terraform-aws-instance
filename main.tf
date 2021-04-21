ad41517963498c7fce072d1dbbb519205a745a5f
Allan <ilegra.allan@hypeflame.com.br>
2021-04-21 12:24:37 -0300

    set count property

--- main.tf
+++ main.tf
@@ -13,9 +13,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
+  count         = 2
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
-
   tags = {
     Name = var.instance_name
   }
